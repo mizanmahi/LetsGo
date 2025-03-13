64d48ff506732edb2377b52c3c27416cbce42308
mizanmahi <mizanmahi24@gmail.com>
2025-03-13 21:48:09 +0600

    pointers done

--- pointers.go
+++ pointers.go
@@ -3,7 +3,10 @@ package main
 // example of pointers use case
 import "fmt"
 
+
 func outputAge(age *int) {
+	// & is used to get the memory address of a variable ex: &age
+	// * is used to get the value from a memory address ex: *age
     *age = 40  // Directly modifies original variable
     fmt.Println("Inside function:", *age)
 }
@@ -12,4 +15,5 @@ func testFunc() {
     age := 32
     outputAge(&age)  // Passing memory address
     fmt.Println("Outside function:", age) // Now modified to 40
+	
 }
