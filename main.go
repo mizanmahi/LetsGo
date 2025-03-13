04b44a889d8d4df7b2b56d14073d59f46b3769ff
mizanmahi <mizanmahi24@gmail.com>
2025-03-13 14:55:15 +0600

    add: file system for storing the balance

--- main.go
+++ main.go
@@ -1,78 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 // fmt is a package that provides I/O functions
 
 // there is basically two types of files in go executable(can be built and produces exe file, must need to have "package main" and a main func) and reusable
 
 func main() {
-	accountBalance := 1000.0
+	accountBalance, _ := readBalanceFromFile()
 
 
-
-
-	fmt.Println("Welcome to Go bank")
-	fmt.Println("Choose an action")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit")
-	fmt.Println("3. Withdraw")
-	fmt.Println("4. Exit")
-
-	var userChoice int
-	fmt.Scan(&userChoice)
-	fmt.Printf("You have selected %d\n", userChoice)
-
-	// if userChoice == 1 {
-	// 	fmt.Println("Your account balance is", accountBalance)
-	// } else if userChoice == 2 {
-	// 	fmt.Println("Enter the amount to deposit")
-	// 	var depositAmount float64
-	// 	fmt.Scan(&depositAmount)
-	// 	accountBalance += depositAmount
-	// 	fmt.Println("Your account balance is", accountBalance)
-	// } else if userChoice == 3 {
-	// 	fmt.Println("Enter the amount to withdraw")
-	// 	var withdrawAmount float64
-	// 	fmt.Scan(&withdrawAmount)
-	// 	if withdrawAmount > accountBalance {
-	// 		fmt.Println("Insufficient balance")
-	// 	} else {
-	// 		accountBalance -= withdrawAmount
-	// 		fmt.Println("Your account balance is", accountBalance)
-	// 	}
-	// 	if withdrawAmount <= 0 {
-	// 		fmt.Println("Invalid amount")
-	// 		return;
-	// 	}
-	// } else if userChoice == 4 {
-	// 	fmt.Println("Thank you for using Go bank")
-	// } else {
-	// 	fmt.Println("Invalid choice")
+	// in GO we have only for loop and while loop is not there
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("Enter your pin: ", i)
 	// }
+
+	// infinite loop, since there is no condition
+	fmt.Println("Welcome to Go bank")
+	 for {
+
+	 	fmt.Println("Choose an action")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw")
+		fmt.Println("4. Exit")
+
+		var userChoice int
+		fmt.Scan(&userChoice)
+		fmt.Printf("You have selected %d\n", userChoice)
+
 
 	// using switch case
 	switch userChoice {
-	case 1:	fmt.Println("Your account balance is", accountBalance)
-	case 2: fmt.Println("Enter the amount to deposit")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		accountBalance += depositAmount
-		fmt.Println("Your account balance is", accountBalance)
-	case 3: fmt.Println("Enter the amount to withdraw")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-		if withdrawAmount > accountBalance {
-			fmt.Println("Insufficient balance")
-		} else {
-			accountBalance -= withdrawAmount
+		case 1:	fmt.Println("Your account balance is", accountBalance)
+		case 2: fmt.Println("Enter the amount to deposit")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			accountBalance += depositAmount
+			saveBalanceToFile(accountBalance)
 			fmt.Println("Your account balance is", accountBalance)
-		}
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid amount")
+		case 3: fmt.Println("Enter the amount to withdraw")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount > accountBalance {
+				fmt.Println("Insufficient balance")
+				continue;
+			} else {
+				accountBalance -= withdrawAmount
+				saveBalanceToFile(accountBalance)
+				fmt.Println("Your account balance is", accountBalance)
+			}
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount")
+				continue;
+			}
+		case 4: fmt.Println("Thank you for using Go bank")
+		   
+			fmt.Println("Your account balance is", accountBalance)
+			 return;
+		default: fmt.Println("Invalid choice")
 			return;
 		}
-	case 4: fmt.Println("Thank you for using Go bank")
-	default: fmt.Println("Invalid choice")
+
 	}
+
+	
+}
+
+func saveBalanceToFile(balance float64) {
+
+	balanceString :=  fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceString), 0644)
+}
+
+func readBalanceFromFile() (float64, error) {
+	balance, _ := os.ReadFile("balance.txt")
+	balanceString := string(balance)
+	return strconv.ParseFloat(balanceString, 64)
+
 }
