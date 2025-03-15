fd6f5abc8fdef4091e95e4a17a8705a4e0faa86b
Mizan Mahi <49094616+mizanmahi@users.noreply.github.com>
2025-03-16 01:20:40 +0600

    done

--- main.go
+++ main.go
@@ -9,15 +9,45 @@ func main() {
 		println("Hello World")
 	}() 
 	// function with parameter
-	func(a int, b int) {
-		println(a + b)
-	}(1, 2) 
+	// func(a int, b int) {
+	// 	println(a + b)
+	// }(1, 2) 
 
-	fmt.Println(super(5))
+	// fmt.Println(super(5))
+
+
+	prices := []int{10,50,60,45,78}
+
+	taxes := [2]float64{5.0, 7.5}
+
+	priceWithTaxedPrice := map[int]int{}
+
+
+	for _, price := range prices {
+		fmt.Println(price)
+		var taxedPrice float64 = float64(price)
+		for _, tax := range taxes {
+			fmt.Println(tax, taxedPrice)
+			taxedPrice += taxedPrice * tax
+		}
+
+		fmt.Println(taxedPrice)
+		priceWithTaxedPrice[price] = int(taxedPrice)
+
+
+	}
+
+	fmt.Println(priceWithTaxedPrice)
+
+
+
+
 	
 	
 	
 }
+
+
 func super(n int) int {
 	if n == 1 {
 		return 1
