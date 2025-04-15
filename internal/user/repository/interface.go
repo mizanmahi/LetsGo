28c988ef4ebfc6edc08d876c6b321475388274c4
Mizan Mahi <49094616+mizanmahi@users.noreply.github.com>
2025-04-15 14:26:47 +0600

    add: the user module

--- internal/user/repository/interface.go
+++ internal/user/repository/interface.go
@@ -1,1 +1,11 @@
-package user
+package repository
+
+import "os/user"
+
+type UserRepository interface {
+	CreateUser(user *user.User) (int, error)
+	GetUserByID(id int) (*user.User, error)
+	UpdateUser(user *user.User) error
+	DeleteUser(id int) error
+	GetAllUsers() ([]*user.User, error)
+}
